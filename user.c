0595eead421fc8f5a2fc8934b1eefd378bf89f24
itzchkm8 <46106321+itzchkm8@users.noreply.github.com>
2019-09-24 17:00:37 +0100

    Update user.c

--- user.c
+++ user.c
@@ -32,14 +32,12 @@ static void parseArgs(long argc, char* const argv[]){
 	}
 }
 
-void reg(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char buffer[], char userID[]) {
-	n = sendto(fd, "reg", 4, 0, res->ai_addr, res->ai_addrlen);
+void reg(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char *buffer) {
+	n = sendto(fd, buffer, strlen(buffer), 0, res->ai_addr, res->ai_addrlen);
 	if(n == -1)
 		exit(1);
 
-	n = sendto(fd, userID, 5, 0, res->ai_addr, res->ai_addrlen);
-	if(n == -1)
-		exit(1);
+	memset(buffer, '\0', sizeof(char)*128);
 
 	addrlen = sizeof(addr);
 	n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
@@ -50,27 +48,58 @@ void reg(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in ad
 	write(1, "\n", 1);
 }
 
-void tl(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char buffer[]) {
-	n = sendto(fd, "tl", 3, 0, res->ai_addr, res->ai_addrlen);
+void topic_list(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char buffer[]) {
+	n = sendto(fd, buffer, strlen(buffer), 0, res->ai_addr, res->ai_addrlen);
 	if(n == -1)
 		exit(1);
+
+	memset(buffer, '\0', sizeof(char)*128);
+
+	addrlen = sizeof(addr);
+	n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
+	if(n == -1)
+		exit(1);
+
+	write(1, buffer, n);
+	write(1, "\n", 1);
 }
 
-void tp(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char buffer[], char userID[]) {
-	n = sendto(fd, "tp", 3, 0, res->ai_addr, res->ai_addrlen);
-	if(n == -1)
-		exit(1);
+void topic_propose(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char *buffer, char userID[], char *topic) {
+	scanf("%s", topic);
+	strcat(buffer, "PTP ");
+	strcat(buffer, userID);
+	strcat(buffer, " ");
+	strcat(buffer, topic);
+	strcat(buffer, "\n");
 
-	n = sendto(fd, userID, 5, 0, res->ai_addr, res->ai_addrlen);
-	if(n == -1)
-		exit(1);
-
-	scanf("%s", buffer);
-	//PROVAVELMENTE ISTO VAI DAR ERRO NO FUTURO STRLEN(BUFFER)
 	n = sendto(fd, buffer, strlen(buffer), 0, res->ai_addr, res->ai_addrlen);
-	//PROVAVELMENTE ISTO VAI DAR ERRO NO FUTURO STRLEN(BUFFER)
 	if(n == -1)
 		exit(1);
+
+	memset(buffer, '\0', sizeof(char)*128);
+	memset(topic, '\0', sizeof(char)*10);
+
+	addrlen = sizeof(addr);
+	n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
+	if(n == -1)
+		exit(1);
+
+	write(1, buffer, n);
+	write(1, "\n", 1);
+}
+
+void question_list(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char *buffer, char *topic) {
+	scanf("%s", topic);
+	strcat(buffer, "LQU ");
+	strcat(buffer, topic);
+	strcat(buffer, "\n");
+
+	n = sendto(fd, buffer, strlen(buffer), 0, res->ai_addr, res->ai_addrlen);
+	if(n == -1)
+		exit(1);
+
+	memset(buffer, '\0', sizeof(char)*128);
+	memset(topic, '\0', sizeof(char)*10);
 
 	addrlen = sizeof(addr);
 	n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
@@ -87,7 +116,8 @@ int main(int argc, char *argv[]){
 	int fd, addrlen, n;
 	struct addrinfo hints, *res;
 	struct sockaddr_in addr;
-	char buffer[128];
+	char *buffer = (char *)malloc(128*sizeof(char));
+	char *topic = (char *)malloc(10*sizeof(char));
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family=AF_INET;
 	hints.ai_socktype=SOCK_DGRAM;
@@ -114,20 +144,27 @@ int main(int argc, char *argv[]){
 	scanf("%s", command);
 	while (strcmp(command, "exit") != 0){
 
+		memset(buffer, '\0', sizeof(char)*128);
+
 		if ( (strcmp(command, "register") == 0) || (strcmp(command, "reg") == 0) ){
 			scanf("%s", userID);
-			reg(fd, addrlen, n, res, addr, buffer, userID);
+			strcat(buffer, "REG ");
+			strcat(buffer, userID);
+			strcat(buffer, "\n");
+			reg(fd, addrlen, n, res, addr, buffer);
 
 		} else if ( (strcmp(command, "topic_list") == 0) || (strcmp(command, "tl") == 0)){
-			tl(fd, addrlen, n, res, addr, buffer);
+			strcat(buffer, "LTP ");
+			strcat(buffer, "\n");
+			topic_list(fd, addrlen, n, res, addr, buffer);
 		} else if (strcmp(command, "topic_select") == 0){
 			
 		} else if (strcmp(command, "ts") == 0){
 			
 		} else if ( (strcmp(command, "topic_propose") == 0) || (strcmp(command, "tp") == 0)){
-			tp(fd, addrlen, n, res, addr, buffer, userID);
+			topic_propose(fd, addrlen, n, res, addr, buffer, userID, topic);
 		} else if ( (strcmp(command, "question_list") == 0) || (strcmp(command, "ql") == 0)){
-			
+			question_list(fd, addrlen, n, res, addr, buffer, topic);
 		} else if (strcmp(command, "question_get") == 0){
 			
 		} else if (strcmp(command, "qg") == 0){
@@ -140,22 +177,8 @@ int main(int argc, char *argv[]){
 		scanf("%s", command);
 	}
 
-
-
-/*
-	n = sendto(fd, "Hello!\n", 7, 0, res->ai_addr, res->ai_addrlen);
-	if(n == -1)
-		exit(1);
-
-	addrlen = sizeof(addr);
-	n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
-	if(n == -1)
-		exit(1);
-
-	write(1, "echo: ", 6);
-	write(1, buffer, n);
-*/
-
+	free(topic);
+	free(buffer);
 	freeaddrinfo(res);
 	close(fd);
 
