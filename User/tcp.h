b8377f78891da54b61063546eb48083347c1128b
pjpe99 <55657866+pjpe99@users.noreply.github.com>
2019-10-15 01:28:11 +0100

    Update tcp.h

--- User/tcp.h
+++ User/tcp.h
@@ -3,7 +3,7 @@
 
 void connectTCP(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr);
 
-void question_get(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char *buffer, char *topic, char *question);
+void question_get(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char *buffer, char* parse, char *topic, char *question);
 
 void question_submit(int fd, int addrlen, int n, struct addrinfo *res, struct sockaddr_in addr, char *buffer, char *parse, char *userID, char *topic, char *question, char *textFile, char *imageFile);
 
