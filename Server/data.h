b9af9e94ef0544a4cdefeab144079a29413f8a08
romanBBNK <bionicletahu@gmail.com>
2019-10-16 20:43:44 +0100

    Data sub-completion

--- Server/data.h
+++ Server/data.h
@@ -41,23 +41,26 @@ struct topic{
 struct topic* addNewTopic(char *Name, char *Author);
 struct topic* getTopic(char *Name);
 void getTopicList(char *buffer);
+int saveNewTopic(char *Name, char *Author);
 
 //Question related functions
 struct question* addNewQuestion(struct topic* parentTopic, char *Title, char *Author, char *imageFilePath);
 struct question* getQuestion(struct topic* parentTopic, char *topicName, char *questionTitle, int questionNumber);
 int getQuestionList(char *buffer, char *topicName);
+int saveNewQuestion(struct topic* parentTopic, char *Title, char *Author, char *imageFilePath);
 
 
 //Answer related functions
-//TODO Add answer functions for adding and getting answers
 struct answer* addNewAnswer(struct question* parentQuestion, char *Name, char *Author, char *imageFilePath);
 struct answer* getAnswer();
 int getAnswerList();
+int saveNewAnswer(char *parentTopic, struct question* parentQuestion, char *Author, char *imageFilePath);
 
-//Miscellaneous functions
+//data storage/retrieval functions
 int retrieveStoredData();
 int retrieveStoredQuestions(struct topic* currentTopic);
 int retrieveStoredAnswers(struct topic* currentTopic ,struct question* currentQuestion);
-void dataInit();
+int generateFilePath(char* topic, char* question, char* answer, char *imgExt, char *targetPath);
+
 
 #endif
